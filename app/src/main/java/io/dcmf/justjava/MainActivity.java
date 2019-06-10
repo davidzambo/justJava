3c93ba95be4ec7f875c72b7960d6508b69e30500
David Zambo <dave@dcmf.io>
2019-06-10 10:30:23 +0200

    Increase/decrease works properly

--- app/src/main/java/io/dcmf/justjava/MainActivity.java
+++ app/src/main/java/io/dcmf/justjava/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +17,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(1);
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increaseQuantity(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+    public void decreaseQuantity(View view) {
+        if (quantity > 0) {
+            quantity--;
+        }
+        display(quantity);
     }
 }
